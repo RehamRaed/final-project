9080fefd12f70305ae135ad15fe1b2c9e877330b
BasmaKuhail <basmakuhail2003@gmail.com>
2025-12-08 14:10:17 +0200

    change lesson border color when chaning status

--- src/components/lesson/Lesson.tsx
+++ src/components/lesson/Lesson.tsx
@@ -3,6 +3,8 @@ import TimeNeeded from "../Course/TimeNeeded"
 import Title from "../Title/title"
 import styles from "./Lesson.module.css"
 import Link from "next/link"
+import Button from "../Button/Button"
+import { useState } from "react"
 
 const lesson = {
         id: 1,
@@ -15,32 +17,46 @@ const lesson = {
         created_at: "2025-01-01 10:00:00",
         updated_at: "2025-01-01 10:00:00",
         timeRequired: 70,
-        status: "Completed",
+        status: "In Progress",
       }
+        let borderColor = "";
 
-    let borderColor = "";
-    if (lesson.status == "Completed"){
-        borderColor = "var(--color-accent)"
-    }else if (lesson.status == "In Progress"){
-        borderColor = "var(--color-primary)"
-    }else{
-        borderColor = "var(--color-border)"
-    }
+
 export default function Lesson({id}:{id:number}){
-    return(<div style={{borderColor:`${borderColor}`}} className={styles.lessonContainer}>
+    
+    const [status, setStatus] = useState(lesson.status)
+    function handleMarkDone(){
+        checkBorderColor()
+        setStatus("Completed")
+        //more to send status to db
+    }
+    function checkBorderColor(){
+        if (status == "Completed"){
+            borderColor = "--color-accent"
+        }else if (status == "In Progress"){
+            borderColor = "--color-primary"
+        }else{
+            borderColor = "--color-border"
+        }
+    }
+    
+    checkBorderColor()
+    return(<div className={styles.lessonContainer}>
         <Link
-        
-        className="flex items-center gap-1 font-semibold transition duration-150"
-        style={{ color: 'var(--color-primary)' }}
-        href={`/course/${lesson.course_id}`}
+            className="flex items-center gap-1 font-semibold transition duration-150"
+            style={{ color: 'var(--color-primary)' }}
+            href={`/course/${lesson.course_id}`}
         >
-        <ArrowLeft size={20} /> Back 
-      </Link>
-        <div className={styles.contentContainer}>
-            <div>
-            <Title title={lesson.title}/>
-            <nav style={{display:"flex", alignItems:"center", color:"var(--color-text-secondary)"}}>(<TimeNeeded minutes={lesson.timeRequired}/>)</nav>
-        </div>
+            <ArrowLeft size={20} /> Back 
+        </Link>
+        <div style={{borderColor:`var(${borderColor})`}} className={styles.contentContainer}>
+            <div className={styles.toppers}>
+                <div>
+                    <Title title={lesson.title}/>
+                    <nav style={{display:"flex", alignItems:"center", color:"var(--color-text-secondary)"}}>(<TimeNeeded minutes={lesson.timeRequired}/>)</nav>
+                </div>
+                <Button title="Mark Done" bgcolor="--color-accent" onClick={handleMarkDone}/>
+            </div>
             <article className={styles.content}>
                 {lesson.content}
             </article>
