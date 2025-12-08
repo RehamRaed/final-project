a4bad7ee350e4e4dc805790d841a0b9eaa20c164
BasmaKuhail <basmakuhail2003@gmail.com>
2025-12-08 13:42:07 +0200

    some structure edit

--- src/app/myroadmap/page.tsx
+++ src/app/myroadmap/page.tsx
@@ -3,9 +3,6 @@ import MyRoadmap from "@/components/StudentRoadmap/StudentRoadmap";
 
 export default function Page() {
   return <>
-    <div style={{zIndex: 50, position: "fixed", top: 0, left: 0, width:"100%"}} >
-      <Header/>
-    </div>
     <MyRoadmap/>
   </>;
 }
