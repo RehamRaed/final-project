32fbea6c3ce355de180f4f1186e59b5eeedf1f93
BasmaKuhail <basmakuhail2003@gmail.com>
2025-12-08 14:57:47 +0200

    edit roadmap style

--- src/components/StudentRoadmap/ProgressBar.tsx
+++ src/components/StudentRoadmap/ProgressBar.tsx
@@ -6,8 +6,11 @@ type progressBarProps ={
 
 export default function ProgressBar({donePersantage}:progressBarProps){
     return(<div className={styles.progressBarContainer}>
-    <p>{donePersantage}% done</p>
     <div className={styles.progressBar}>
         <div style={{width:`${donePersantage}%`}} className={styles.progress}></div>
     </div>
+    <div style={{display:"flex", justifyContent:"flex-end"}}>
+        <p style={{color:"var(--color-text-secondary)", fontSize:"var(--font-xs)"}}>{donePersantage}% done</p>
+    </div>
+    
 </div>)}
